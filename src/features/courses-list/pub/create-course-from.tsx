6d56b2f291fb9970be941b4cdaba8cb12807cea6
taneros <taneros@duck.com>
2025-03-02 07:15:32 +0300

    fix: test deploy

--- src/features/courses-list/pub/create-course-from.tsx
+++ src/features/courses-list/pub/create-course-from.tsx
@@ -49,7 +49,7 @@ export const CreateCourseForm = ({className, revalidatePagePath }: ICreateCourse
             <FormItem>
               <FormLabel>Название</FormLabel>
               <FormControl>
-                <Input placeholder="Название" {...field} />
+                <Input placeholder="Название -- " {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
